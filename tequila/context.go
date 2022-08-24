2946b21f1920ea18e1c1afd1d41bbc083129aab0
KieSun <zx597813039@gmail.com>
2022-08-24 10:27:26 +0800

    json

--- tequila/context.go
+++ tequila/context.go
@@ -1,8 +1,24 @@
 package tequila
 
-import "net/http"
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
 
 type Context struct {
 	W http.ResponseWriter
 	R *http.Request
+}
+
+func (ctx *Context) Json(status int, data any) error {
+	jsonData, err := json.Marshal(data)
+	fmt.Print(jsonData)
+	if err != nil {
+		return err
+	}
+	ctx.W.Header().Set("Content-type", "application/json; charset=utf-8")
+	ctx.W.WriteHeader(status)
+	_, err = ctx.W.Write(jsonData)
+	return err
 }
--- test/main.go
+++ test/main.go
@@ -3,7 +3,12 @@ package main
 import (
 	"fmt"
 	"github/KieSun/Tequila/tequila"
+	"net/http"
 )
+
+type User struct {
+	Name string
+}
 
 func main() {
 	engine := tequila.New()
@@ -13,12 +18,21 @@ func main() {
 	//})
 	user.Use(func(handlerFunc tequila.HandlerFunc) tequila.HandlerFunc {
 		return func(ctx *tequila.Context) {
-			fmt.Printf("middleware")
+			handlerFunc(ctx)
+		}
+	})
+	user.Use(func(handlerFunc tequila.HandlerFunc) tequila.HandlerFunc {
+		return func(ctx *tequila.Context) {
 			handlerFunc(ctx)
 		}
 	})
 	user.Get("/:id", func(c *tequila.Context) {
+		err := c.Json(http.StatusOK, &User{Name: "yck"})
+		if err != nil {
+			fmt.Print(err)
+		}
 		fmt.Printf("get")
 	})
+
 	engine.Run()
 }
